e609536ad8b20105ef4e23af9e298f7ae362b283
Hermeskid123 <preston.mann@mavs.uta.edu>
2023-02-24 11:30:19 -0600

    repo set up

---
+++ README.md
@@ -0,0 +1,23 @@
+Spring 2023 hand pose project 
+VLM lab under Dr. Athisos 
+
+Team:
+Preston Mann
+Rodrigo Hernandez-Pleite 
+
+TODO:
+set up repo 
+make project summary
+make main
+
+phase 1:
+build quary system
+
+phase 2:
+build key point system for a single img
+
+phase 3:
+real time key point system for x frams per second 
+
+
+Project Summary:
---
+++ main.py
@@ -0,0 +1,28 @@
+#============================================================
+#
+#                           VLM
+#               CSE Reserch Project Spring 2023
+#               The University of Texas at Arlington
+#               Rodrigo Hernandez-Pleite, Preston Mann
+#
+#
+#------------------------------------------------------------
+#
+# File: main.py
+#
+# Brief: This file is our main it will run our program 
+# TODO: add more to this Breif 
+#         
+#
+# Revision History:
+# Date       | Author       | Description
+# -----------|--------------|--------------------------------
+# 02-24-2023 | Mann, P.     | Initial Release
+#============================================================
+
+def main():
+    print("This is the main")
+
+
+if __name__ == "__main__":
+    main()
