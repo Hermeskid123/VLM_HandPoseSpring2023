a1b73b46e8c96411266f9320d83719e1ac3ad4ab
rodPleitez <86507039+rodPleitez@users.noreply.github.com>
2023-04-12 21:44:49 -0500

    Add files via upload

--- knn/knn_code.py
+++ knn/knn_code.py
@@ -1,58 +1,22 @@
-from keras.datasets import mnist
-from matplotlib import pyplot
+from chamfer_distance import *
 import numpy as np
-import utility as u
-import distance_functions as df
-import time
+import os
+import cv2
 
-def classification(trainSet, testSet, distanceFunction):
-    classifications = []
-    for i in range(len(testSet[0])):
-        classifications.append(nearest_neighbor(trainSet, testSet[0][i], distanceFunction))
-    #print(classifications)
-    classAccuracy = 0
-    for i in range(len(classifications)):
-        accuracy = 0
-        if classifications[i] == testSet[1][i]:
-            accuracy = 1
-        print("Id:%5d, predicted=%s, actual=%s, accuracy=%6.4f" % (i+1, classifications[i], testSet[1][i], accuracy))
-        
-        classAccuracy = classAccuracy + accuracy
-    print("Classification accuracy=%6.4f" % (classAccuracy))
+def knn(img, datasetPath, k):
+    distances = []
+    filenames = os.listdir(datasetPath)
 
-    return classAccuracy / len(testSet[0])
+    for filename in filenames:
+        image = cv2.imread(datasetPath + "/" + filename)
+        chamferDistance = chamfer_distance(img, image)
+        distances.append(chamferDistance)
+    
+    distances = np.asarray(distances)
 
-def nearest_neighbor(trainingInputs, testInput, distanceFunction):
-    distanceList = []
-    for i in range(len(trainingInputs[0])):
-        distanceList.append(distanceFunction(testInput, trainingInputs[0][i]))
-        
-    minDistance = np.argmin(distanceList)
-    nearestNeighborClassification = trainingInputs[1][minDistance]
+    kNearest = np.argsort(distances)
 
-    return nearestNeighborClassification
+    kNearestNeighbors = kNearest[0:k]
 
-
-def training(newTrainSet, testSet, distanceFunction, name, n, flag, mpegFlag):
-    acc = []
-    times = []
-    for i in range(n):
-        newTestSet = u.create_new_test_set(testSet, 10)
-
-        if flag == "edge":
-            newTestSet = u.edge_detection_set(newTestSet, mpegFlag)
-        elif flag == "binary":
-            newTestSet = u.binary_set(newTestSet)
-
-        start  = time.time()
-        acc.append(classification(newTrainSet, newTestSet, distanceFunction))
-        end = time.time()
-        times.append(end - start)
-
-    acc = sum(acc) / len(acc)
-    times = sum(times) / len(times)
-    print("Average of accuracies for", name, "data =", acc)
-    print("Average of times for", name, "data =", times)
-
-    return (acc, times)
+    return kNearestNeighbors
 
